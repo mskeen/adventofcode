66bcfac45b892c607fb33e2b5bc0aa8f79073c6f
Mike Skeen <skeen.mike@gmail.com>
2015-12-17 15:32:11 -0500

    Day 17 solution

---
+++ day17/eggnog.rb
@@ -0,0 +1,31 @@
+@source_buckets = [11, 30, 47, 31, 32, 36, 3, 1, 5, 3, 32, 36, 15, 11, 46, 26, 28, 1, 19, 3]
+@target = 150
+@bits = @source_buckets.size
+
+total_count = best_bit_count_freq = 0
+best_bit_count = @bits
+
+(1..(2**@bits - 1)).each do |i|
+  sum = bit_count = 0
+  bit_count = 0
+  (0..(@bits - 1)).each do |index|
+    bit = 2 ** index
+    if (bit & i) > 0
+      sum += @source_buckets[index]
+      bit_count += 1
+    end
+  end
+  if sum == @target
+    total_count += 1
+    if bit_count == best_bit_count
+      best_bit_count_freq += 1
+    elsif bit_count < best_bit_count
+      best_bit_count_freq = 1
+      best_bit_count = bit_count
+    end
+  end
+end
+
+puts total_count
+
+puts "#{best_bit_count} containers were used in #{best_bit_count_freq} combinations"
